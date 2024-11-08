de73d46ab5294f09f54b05d9347946ed71bc2c42
sanjangithub <sanjan.chikkala@gmail.com>
2024-11-09 00:48:05 +0530

    Remove git file and added another java practice code file

---
+++ SomeStuff.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class SomeStuff {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3); numbers.add(5); numbers.add(710);
+
+        numbers.stream().map(i -> i*8).map(i -> i/3).forEach(System.out::println);
+    }
+}
